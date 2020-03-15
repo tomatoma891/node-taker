fcf8e133fba4ef90a72436fb1c13152e825fb283
Tamara Sidorova <tomatoma891@gmail.com>
2020-03-15 14:52:59 -0600

    fixed api routes

---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
+
